f5cdf957a485862cdd9b5594eef23f1e660c7c4e
Tiago Balbino Ribeiro <tiago.ribeiro@luizalabs.com>
2024-04-26 10:49:46 -0300

    feat(users): adiciona rota para criar usuários

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.post('/users', (req, res) => {
+    // lgica para criar usuário
+    res.json({ message: 'Usuário criado com sucesso' })
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
