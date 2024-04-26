83a4907da0c9d9069209c02eaff5e240347a65df
Tiago Balbino Ribeiro <tiago.ribeiro@luizalabs.com>
2024-04-26 12:18:19 -0300

    rafactor(users) - lógica de criação de usuário extrida para uma função

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function criarUsuario(body){
+    return { nome: 'usuario test', id: 1}
+}
+
 app.post('/users', (req, res) => {
-    // lgica para criar usuário
-    res.json({ message: 'Usuário criado com sucesso' })
+    const usuario = criarUsuario(req.body)
+
+    res.json(usuario)
 })
 
 app.listen(port, () => {
